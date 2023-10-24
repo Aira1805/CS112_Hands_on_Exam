f6e6a8b9672d0798daba43b83c4163408fa5ab5f
AIRA <144906782+Aira1805@users.noreply.github.com>
2023-10-24 23:49:28 +0800

    Add files via upload

---
+++ CS112_Hands_on_Exam.py
@@ -0,0 +1,13 @@
+decimal_number = 1  # The decimal number you want to convert to binary
+binary_number = bin(decimal_number)  # Convert to binary
+print("Binary representation:", binary_number)
+
+
+decimal_number = 2  # The decimal number you want to convert to octal
+octal_number = oct(decimal_number)  # Convert to octal
+print("Octal representation:", octal_number)
+
+
+decimal_number = 3  # The decimal number you want to convert to hexadecimal
+hexadecimal_number = hex(decimal_number)  # Convert to hexadecimal
+print("Hexadecimal representation:", hexadecimal_number)
